ab01907c37a929ee60a147d1e817789477dd0d0d
FerreWagner <18408229270@163.com>
2017-12-22 16:18:23 +0800

    add admin edit && switch function

--- app/admin/controller/Admin.php
+++ app/admin/controller/Admin.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 
 use app\admin\common\Base;
+use app\admin\model\Admin as AdminModel;
+use think\Request;
 
 
 class Admin extends Base
@@ -15,11 +17,47 @@ class Admin extends Base
      */
     public function index()
     {
-        $ferre = db('admin')->find(1);
-        $this->view->assign('ferre', $ferre);
-        return $this->view->fetch('admin_list');
+        
+        $admin = AdminModel::all();
+        $this->view->assign('admin', $admin);
+        return $this->view->fetch('admin-list');
         
     }
+    
+    /**
+     * 
+     * @param Request $request
+     * @param unknown $id
+     * @return string
+     */
+    public function edit(Request $request, $id)
+    {
+        $admin = AdminModel::find(request()->input($id));
+        
+        if ($request->isPost()){
+            $update = $request->param();
+            $update['password'] = empty($update['password']) ? $admin['password'] : $update['password'];
+            
+            $res = AdminModel::update($update);
+            $res ? $this->redirect('admin/admin/index') : $this->error('数据更新失败');
+        }
+        
+        
+        $this->view->assign('admin', $admin);
+        return $this->view->fetch('admin-edit');
+    }
+    
+    public function trans($id)
+    {
+        $admin = AdminModel::where('id', $id)->field(['switch', 'id'])->find(1);
+        $admin['switch'] = $admin['switch'] == 'true' ? 'false' : 'true';
+        $res = AdminModel::update(['switch' => $admin['switch']], ['id' => $admin['id']]); //update
+        
+        $res ? $this->redirect('admin/admin/index') : $this->error('更新失败');
+        
+    }
+    
+    
     
     /**
      * admin log data list
@@ -30,5 +68,7 @@ class Admin extends Base
         $this->view->assign('alog', $log_data);
         return $this->view->fetch('admin-log');
     }
+    
+
     
 }
