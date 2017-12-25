84bf9ea5853adefa9e3bdda1f5b3dd2a59bb5505
FerreWagner <18408229270@163.com>
2017-12-25 17:57:41 +0800

    view opt

--- app/admin/controller/Article.php
+++ app/admin/controller/Article.php
@@ -17,7 +17,7 @@ class Article extends Base
      */
     public function index(Request $request)
     {
-        $article = db('article')->field('a.*,b.catename')->alias('a')->join('alexa_category b','a.cate=b.id')->order('a.id desc')->paginate(6);
+        $article = db('article')->field('a.*,b.catename')->alias('a')->join('alexa_category b','a.cate=b.id')->order('a.id desc')->paginate(10);
         
         //search function
         if ($request->isPost()){
@@ -25,12 +25,12 @@ class Article extends Base
             
             if (empty($search['start']) || empty($search['end'])){
                 $article = db('article')->field('a.*,b.catename')->alias('a')->join('alexa_category b','a.cate=b.id')->order('a.id desc')
-                                        ->where('title', 'like', '%'.$search['title'].'%')->paginate(6);
+                                        ->where('title', 'like', '%'.$search['title'].'%')->paginate(10);
                 
             }else {
                 $article = db('article')->field('a.*,b.catename')->alias('a')->join('alexa_category b','a.cate=b.id')->order('a.id desc')
                                         ->where('time', 'between', [strtotime($search['start']), strtotime($search['end'])])
-                                        ->where('title', 'like', '%'.$search['title'].'%')->paginate(6);
+                                        ->where('title', 'like', '%'.$search['title'].'%')->paginate(10);
                 
             }
         }
