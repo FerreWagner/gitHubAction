d6bdbdce0df17ca1d8c132fb01316678d126c792
FerreWagner <18408229270@163.com>
2018-01-08 11:59:39 +0800

    add open mail server function

--- app/admin/controller/Mailer.php
+++ app/admin/controller/Mailer.php
@@ -8,7 +8,7 @@ class Mailer extends Mail
     public function admin()
     {
         if (request()->isPost()){
-        
+            file_put_contents('dbconfig.php', input('post.'));
 //             halt(request()->param());
             $this->init();
             $this->content();
@@ -22,5 +22,11 @@ class Mailer extends Mail
         }
         
         return $this->view->fetch();
+    }
+    
+    public function test()
+    {
+        $test = request()->getInput();
+        halt($test);
     }
 }
